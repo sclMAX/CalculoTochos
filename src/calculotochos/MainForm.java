ab03038c2ae62dea5bd27900174c02afef1aed61
sclMAX <maxisoftman@gmail.com>
2014-01-28 20:07:34 -0300

    Desarrollo

--- src/calculotochos/MainForm.java
+++ src/calculotochos/MainForm.java
@@ -695,6 +695,7 @@ public class MainForm extends javax.swing.JFrame {
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
             public void run() {
                 new MainForm().setVisible(true);
             }
